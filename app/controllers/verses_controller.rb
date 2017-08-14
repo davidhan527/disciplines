1f03fa9d0af6b840ee2961b10742c01b1c08c360
David Han <davidhan527@gmail.com>
2017-08-14 15:22:19 -0400

    Get and display verses

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -1,2 +1,31 @@
 class VersesController < ApplicationController
+  def index
+  end
+
+  def text
+    @text = VerseFinder.new(params[:verse]).verses
+    render :index
+  end
+
+  private
+
+  class VerseFinder
+    include HTTParty
+    base_uri 'www.esvapi.org'
+
+    def initialize(passage)
+      @options = { query: default_params.merge(passage: passage) }
+    end
+
+    def default_params
+      {
+        key: 'IP',
+        'include-footnotes': false,
+      }
+    end
+
+    def verses
+      self.class.get("/v2/rest/passageQuery", @options)
+    end
+  end
 end
