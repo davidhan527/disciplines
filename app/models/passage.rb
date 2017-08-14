a545d07072f49c2d4a3c9fc116aa1fa7a6b0ae12
David Han <davidhan527@gmail.com>
2017-08-14 18:38:31 -0400

    Add AR relationships

--- app/models/passage.rb
+++ app/models/passage.rb
@@ -1,4 +1,6 @@
 class Passage < ApplicationRecord
   has_many :cards
+  has_many :users, through: :cards
+
   validates :reference, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   include Clearance::User
+  has_many :cards
+  has_many :passages, through: :cards
 end
