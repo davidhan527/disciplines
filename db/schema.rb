bb2964de36bf7d2efa277384812da802e6d08c5b
David Han <davidhan527@gmail.com>
2017-08-14 17:57:15 -0400

    db:migrate create cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814205003) do
+ActiveRecord::Schema.define(version: 20170814215429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,4 +33,5 @@ ActiveRecord::Schema.define(version: 20170814205003) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "cards", "users"
 end
